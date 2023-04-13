0812e8416c10a95671155e9dca9ca237fb158873
Struan <60407763+stustr@users.noreply.github.com>
2023-04-13 10:15:08 +0100

    Make superclass Money and abstract logic into it

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -1,15 +1,6 @@
-public class Franc {
-    private int amount;
+public class Franc extends Money{
 
     public Franc(int amount) {
-        this.amount = amount;
-    }
-    Franc times(int multiplier) {
-        return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc dollar = (Franc) object;
-        return amount == dollar.amount;
+        super(amount);
     }
 }
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,2 +1,15 @@
 public class Money {
+    protected int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+    Money times(int multiplier) {
+        return new Money(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
