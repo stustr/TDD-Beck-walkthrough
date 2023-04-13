5066a5639796e1bd72986dbb6a9dac27bb0fd8c0
Struan <60407763+stustr@users.noreply.github.com>
2023-04-13 09:51:19 +0100

    :test_tube: MoneyTest: testEquality added

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,9 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
+
+    @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
