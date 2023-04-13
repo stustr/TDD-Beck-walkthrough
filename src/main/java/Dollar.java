60d32638129aa18e6f130fef9b11337813660d50
Struan <60407763+stustr@users.noreply.github.com>
2023-04-13 09:50:33 +0100

    Dollar: amount made private (abstraction)

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,2 +1,15 @@
 public class Dollar {
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
 }
